261b5c6cd00ea50fd90567049ccf97678e56f5c2
Mark Ashworth <mp.ashworth@gmail.com>
2017-08-05 06:33:00 +0200

    Package renaming :0)

--- core/src/main/java/io/threesixty/compare/result/AbstractResultRecord.java
+++ core/src/main/java/io/threesixty/compare/result/AbstractResultRecord.java
@@ -1,6 +1,6 @@
-package io.threesixty.kt.core.result;
+package io.threesixty.compare.result;
 
-import io.threesixty.kt.core.DataRecord;
+import io.threesixty.compare.DataRecord;
 
 /**
  * @author Mark P Ashworth (mp.ashworth@gmail.com)
